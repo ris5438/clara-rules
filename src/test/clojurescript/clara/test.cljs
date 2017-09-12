56a3e6d37ba9a12aa84ce1951b867cfbc1f266dc
ris5438 <thrill4rishabh@gmail.com>
2017-09-12 14:03:52 -0500

    Revert "Rebase"

--- src/test/clojurescript/clara/test.cljs
+++ src/test/clojurescript/clara/test.cljs
@@ -16,29 +16,21 @@
 
 (enable-console-print!)
 
-(def ^:dynamic *successful?* nil)
-
 (defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
   (if (cljs.test/successful? m)
-    (do
-      (println "Success!")
-      (reset! *successful?* true))
-    (do
-      (println "FAIL")
-      (reset! *successful?* false))))
+    (println "Success!")
+    (println "FAIL")))
 
 (defn ^:export run []
-  (binding [*successful?* (atom nil)]
-    (test/run-tests 'clara.test-rules
-                    'clara.test-common
-                    'clara.test-salience
-                    'clara.test-testing-utils
-                    'clara.test-complex-negation
-                    'clara.test-accumulators
-                    'clara.test-exists
-                    'clara.tools.test-tracing
-                    'clara.test-truth-maintenance
-                    'clara.test-dsl
-                    'clara.test-accumulation
-                    'clara.test-memory)
-    @*successful?*))
+  (test/run-tests 'clara.test-rules
+                  'clara.test-common
+                  'clara.test-salience
+                  'clara.test-testing-utils
+                  'clara.test-complex-negation
+                  'clara.test-accumulators
+                  'clara.test-exists
+                  'clara.tools.test-tracing
+                  'clara.test-truth-maintenance
+                  'clara.test-dsl
+                  'clara.test-accumulation
+                  'clara.test-memory))
